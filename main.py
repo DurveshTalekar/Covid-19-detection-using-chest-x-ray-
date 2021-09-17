64181f31aa68ca696805b799df799bf7c8f3f2ee
DurveshTalekar <84030149+DurveshTalekar@users.noreply.github.com>
2021-09-17 19:52:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def main():
         welcome() 
     if selected_box == 'Image Processing 1':
         photo()
-    if selected_box == 'Image Processing 2':
-        photo1()
-    if selected_box == 'Image Preprocessing':
-        photo3()
-    if selected_box == 'Detection':
-        feature_detection()
+#     if selected_box == 'Image Processing 2':
+#         photo1()
+#     if selected_box == 'Image Preprocessing':
+#         photo3()
+#     if selected_box == 'Detection':
+#         feature_detection()
  
 
 def welcome():
@@ -89,47 +89,47 @@ def photo():
             
             #im = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
             clahe = cv2.createCLAHE(clipLimit = 4)
-            final_img = clahe.apply(final_img) 
-            final_img = cv2.resize(final_img, (256, 256))
+            final_img1 = clahe.apply(final_img) 
+            final_img2 = cv2.resize(final_img1, (256, 256))
             st.write("")
             st.write("")
             st.write("After applying CLAHE:")
             st.write("")
       
-            st.image(final_img)
+            st.image(final_img2)
         else:
             st.write("Make sure you image is in JPG/PNG Format.")
     
-def photo1():
+# def photo1():
         
         
-        def load_image(img):
-            im = Image.open(img)
-            image = np.array(im)
-            return image
+#         def load_image(img):
+#             im = Image.open(img)
+#             image = np.array(im)
+#             return image
 
-        uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png', 'jpeg'])
+#         uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png', 'jpeg'])
 
-        if uploadFile is not None:
-            st.write("Original X-ray Image:")
-            st.write("")
-            img = load_image(uploadFile)
-            img = cv2.resize(img, (400, 400))
+#         if uploadFile is not None:
+#             st.write("Original X-ray Image:")
+#             st.write("")
+#             img = load_image(uploadFile)
+#             img = cv2.resize(img, (400, 400))
             
-            x = st.slider('Change Threshold value',min_value = 69,max_value = 169)
-            image = cv2.imread('image.png')
-            image = cv2.resize(img, (300, 300))
-            ret,thresh1 = cv2.threshold(image,x,255,cv2.THRESH_BINARY)
-            thresh1 = thresh1.astype(np.float64)
-            st.image(thresh1, use_column_width=True,clamp = True)
+#             x = st.slider('Change Threshold value',min_value = 69,max_value = 169)
+#             image = cv2.imread('image.png')
+#             image = cv2.resize(img, (300, 300))
+#             ret,thresh1 = cv2.threshold(image,x,255,cv2.THRESH_BINARY)
+#             thresh1 = thresh1.astype(np.float64)
+#             st.image(thresh1, use_column_width=True,clamp = True)
     
-            st.text("Bar Chart of the image")
-            histr = cv2.calcHist([image],[0],None,[256],[0,256])
-            st.bar_chart(histr)
+#             st.text("Bar Chart of the image")
+#             histr = cv2.calcHist([image],[0],None,[256],[0,256])
+#             st.bar_chart(histr)
       
-            st.image(final_img)
-        else:
-            st.write("Make sure you image is in JPG/PNG Format.")
+#             st.image(final_img)
+#         else:
+#             st.write("Make sure you image is in JPG/PNG Format.")
             
             
 
